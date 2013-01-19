cd9380e64c3d5be42ea10bf34dbfce0120923d9e
xuwei-k <6b656e6a69@gmail.com>
2013-01-19 17:10:13 +0900

    unidoc
    Conflicts:
    	project/Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,8 @@ import sbt._,Keys._
 
 object build extends Build{
 
+  val scaladocBranch = TaskKey[String]("scaladoc-branch")
+
   val baseSettings = ScctPlugin.instrumentSettings ++ Seq(
     organization := "com.github.kmizu",
     version := "0.6.0",
@@ -15,6 +17,10 @@ object build extends Build{
       Opts.resolver.sonatypeReleases
     ),
     scalacOptions ++= Seq("-deprecation","-unchecked"),
+    scaladocBranch := "master",
+    scalacOptions in (Compile, doc) <++= (baseDirectory, scaladocBranch).map{ (bd, branch) =>
+      Seq("-sourcepath", bd.getAbsolutePath, "-doc-source-url", "https://github.com/kmizu/jsonda/tree/" + branch + "€{FILE_PATH}.scala")
+    },
     testOptions += Tests.Argument(TestFrameworks.Specs2, "console", "junitxml"),
     publishMavenStyle := true,
     publishTo <<= version { (v: String) =>
@@ -53,7 +59,7 @@ object build extends Build{
     "jsonda",
     file(".")
   ).settings(
-    baseSettings ++ Seq(
+    baseSettings ++ Unidoc.settings ++ Seq(
       publishArtifact := false, publish := {}, publishLocal := {}
     ) : _*
   ).aggregate(
